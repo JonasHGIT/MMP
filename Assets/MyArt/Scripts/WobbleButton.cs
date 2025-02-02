a7a8a741074ad0916ddc4ed6891c81c80110a526
JonasHGIT <jonas.ha2000@web.de>
2025-02-02 01:40:36 +0100

    Added: KI Vorlesen für Szene 0 und 1, Jonas Vorlesen für Szene 5; Einige neue Assets wurden geadded, Szenen betroffen 0, 1 und 5

--- Assets/MyArt/Scripts/WobbleButton.cs
+++ Assets/MyArt/Scripts/WobbleButton.cs
@@ -11,16 +11,29 @@ public class WobbleButton : MonoBehaviour
         // Alle Objekte mit dem Tag "Interactable" suchen
         GameObject[] interactables = GameObject.FindGameObjectsWithTag("Interactable");
 
+        Debug.Log($"[HighlightInteractables] Anzahl der gefundenen Interactable-Objekte: {interactables.Length}");
+
         foreach (GameObject obj in interactables)
         {
             // Überprüfen, ob das Objekt zur aktuellen Szene gehört
             if (obj.scene == currentScene)
             {
+                Debug.Log($"[HighlightInteractables] Objekt gefunden: {obj.name}, Position: {obj.transform.position}");
+                
                 WobbleEffect wobble = obj.GetComponent<WobbleEffect>();
                 if (wobble != null)
                 {
                     wobble.StartWobble();
+                    Debug.Log($"[HighlightInteractables] WobbleEffect gestartet für Objekt: {obj.name}");
                 }
+                else
+                {
+                    Debug.LogWarning($"[HighlightInteractables] Kein WobbleEffect an Objekt: {obj.name}");
+                }
+            }
+            else
+            {
+                Debug.Log($"[HighlightInteractables] Objekt {obj.name} gehört nicht zur aktuellen Szene.");
             }
         }
     }
